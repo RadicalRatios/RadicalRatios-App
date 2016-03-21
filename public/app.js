82287d3b40ff549c37691742d00b7f34500a4020
Jake Widmer <jwidmer@penrodsoftware.com>
2016-03-20 21:10:44 -0500

    gut out 1

--- public/app.js
+++ public/app.js
@@ -1,27 +1,18 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('TrukSpot', ['ngRoute', 'ngResource', 'myApp.version',
-    'RadicalRatios.home','TrukSpot.about', 'TrukSpot.user', 'TrukSpot.myTruck',
-    'TrukSpot.services', 'RadicalRatios.instructor', 'Quintus', 'RadicalRatios.template',
+angular.module('RadicalRatios', ['ngRoute', 'ngResource', 'myApp.version',
+    'RadicalRatios.home', 'RadicalRatios.instructor', 'Quintus', 'RadicalRatios.template',
     'RadicalRatios.game','RadicalRatios.game.game3','RadicalRatios.game.game4', 'RadicalRatios.game.options',
     'RadicalRatios.game.options.linkSession'
 ])
     /**
      *  Non-authed routes:
      */
-
     .config(['$routeProvider', function($routeProvider) {
         $routeProvider.when('/home', {
             templateUrl: 'home/templates/home.html',
             controller: 'HomeController'
-        });
-    }])
-
-    .config(['$routeProvider', function($routeProvider) {
-        $routeProvider.when('/about', {
-            templateUrl: 'about/about.html',
-            controller: 'AboutCtrl'
         });
     }])
 
@@ -78,53 +69,10 @@ angular.module('TrukSpot', ['ngRoute', 'ngResource', 'myApp.version',
     //    QProvider.include("Sprites, Scenes, 2D, Input").setup({ width: 800, height: 400 });
     //})
 
-
-    /* TODO: Enable when the app goes v1 */
-    //.config(['$routeProvider', function($routeProvider) {
-    //    $routeProvider.when('/home', {
-    //        templateUrl: 'home/home.html',
-    //        controller: 'HomeCtrl'
-    //    });
-    //}])
-    //
-    //.config(['$routeProvider', function($routeProvider) {
-    //    $routeProvider.when('/login', {
-    //        templateUrl: 'user/loginAccount.html',
-    //        controller: 'LoginAccountCtrl'
-    //    });
-    //}])
-    //
-    //.config(['$routeProvider', function($routeProvider) {
-    //    $routeProvider.when('/create-account', {
-    //        templateUrl: 'user/createAccount.html',
-    //        controller: 'CreateAccountCtrl'
-    //    });
-    //}])
-    //
-    //
-    ///**
-    // *  Authed routes:
-    // */
-    //
-    //.config(['$routeProvider', function($routeProvider) {
-    //    $routeProvider.when('/my-truck', {
-    //        templateUrl: 'myTruck/myTruck.html',
-    //        controller: 'MyTruckCtrl'
-    //    });
-    //}])
-
     /**
      *  Default route:
      */
-
     .config(['$routeProvider', '$resourceProvider', function($routeProvider, $resourceProvider) {
         $routeProvider.otherwise({redirectTo: '/home'});
         $resourceProvider.defaults.stripTrailingSlashes = false;
     }]);
-
-angular.module('TrukSpot.services', ['ngResource'])
-
-    .factory('EmailContact', ['$resource', function($resource) {
-        return $resource('/api/contact');
-    }]);
-
