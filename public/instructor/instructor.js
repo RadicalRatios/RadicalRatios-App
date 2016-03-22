372e7406ceda637c9cdc3aeb1d6369d81b6d81f9
Brian Ziebart <bziebart@yahoo.com>
2016-03-21 21:00:16 -0500

    instructor work

--- public/instructor/instructor.js
+++ public/instructor/instructor.js
@@ -4,12 +4,37 @@ angular.module('RadicalRatios.instructor', [
     'Quintus'
     ])
     .controller('InstructorController', ['$scope', '$location', function($scope, $location){
-        $scope.name = "Instructor Page";
+
+        $scope.sessionKey = null;
+        $scope.instructorName = null;
+        $scope.instructorEmail = null;
+        $scope.instructorPassword = null;
+        $scope.buttonDisabled = null;
+        $scope.keyMade = false;
+
+
+
+        $scope.generateKey = function(){
+
+                $scope.sessionKey = generateID();
+                $scope.keyMade = true;
+        }
 
         $scope.navBack = function(){
             $location.path( "/home" );
         }
 
+
+        function generateID()
+        {
+            var text = "";
+            var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+            for( var i=0; i < 5; i++ )
+                text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+            return text;
+        }
 
 
     }]);
