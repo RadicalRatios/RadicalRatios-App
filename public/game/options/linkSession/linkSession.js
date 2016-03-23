0185eaf4c9e8bf0a9928140b167fe9ff3f1b0ce5
Brian Ziebart <bziebart@yahoo.com>
2016-03-22 21:37:05 -0500

    link session framework up

--- public/game/options/linkSession/linkSession.js
+++ public/game/options/linkSession/linkSession.js
@@ -3,9 +3,36 @@
 angular.module('RadicalRatios.game.options.linkSession', ['ngRoute'])
 
     .controller('LinkSessionController',['$scope', '$location', function($scope, $location){
-        $scope.name = "Link Session to Instructor";
+
+        $scope.instructorKey = null;
+        $scope.studentName = null;
+        $scope.inputError = false;
+
+        function isEmpty(str) {
+            return (!str || 0 === str.length);
+        }
+
+        $scope.linkKey = function(){
+            if(isEmpty($scope.studentName) || isEmpty($scope.instructorKey)){
+                $scope.keyLinked = false;
+                $scope.inputError = true;
+            }
+            else {
+                $scope.inputError = false;
+                $scope.keyLinked = true;
+
+                $scope.clearInputs();
+            }
+        }
+
+        $scope.clearInputs = function(){
+            $scope.instructorKey = null;
+            $scope.studentName = null;
+        }
 
         $scope.navBack = function(){
             $location.path( "/game/options" );
         }
+
+
     }]);
