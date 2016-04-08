34c2b6b3a1cbdcc7e61a9292975817ee4eae0ccd
Brian Ziebart <bziebart@yahoo.com>
2016-04-08 15:54:02 -0500

    cosmetics and game example preview

--- public/home/home.js
+++ public/home/home.js
@@ -5,18 +5,9 @@ angular.module('RadicalRatios.home', ['ngRoute'])
     .controller('HomeController',['$scope', '$location', function($scope, $location){
         $scope.name = "Welcome to Radical Ratios!";
 
-
         $scope.navToGame = function(){
             $location.path( "/game" );
         }
-
-
-
-
-
-
-
-
 
 
     }]);
