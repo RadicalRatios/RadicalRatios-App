57e4108b7a330ad7fbaf5dd1f5f8a4f88172f5d7
Brian Ziebart <bziebart@yahoo.com>
2016-04-09 16:07:29 -0500

    endgame modal

--- public/answer/gameover.js
+++ public/answer/gameover.js
@@ -4,5 +4,30 @@ angular.module('RadicalRatios.gameover', [
     ])
     .controller('GameoverController', ['$scope', 'score', function($scope, score){
         $scope.score = score;
+        if(score < 4) {
+            $scope.starType1 = "glyphicon-star-empty";
+            $scope.starType2 = "glyphicon-star-empty";
+            $scope.starType3 = "glyphicon-star-empty";
+        }
+        else if(score < 7){
+            $scope.starType1 = "glyphicon-star";
+            $scope.starType2 = "glyphicon-star-empty";
+            $scope.starType3 = "glyphicon-star-empty";
+        }
+        else if (score < 9){
+            $scope.starType1 = "glyphicon-star";
+            $scope.starType2 = "glyphicon-star";
+            $scope.starType3 = "glyphicon-star-empty";
+        }
+        else{
+            $scope.starType1 = "glyphicon-star";
+            $scope.starType2 = "glyphicon-star";
+            $scope.starType3 = "glyphicon-star";
+        }
+
+        //var x = document.getElementById("starsDiv");
+        //var y = document.createElement('span');
+        //y.className = "glyphicon glyphicon-star-empty star";
+        //x.appendChild(y);
 
     }]);
